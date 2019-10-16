d601135c684934dbb6860afa928c21d391df4775
ojustine <vladimir.chernyak.91@gmail.com>
2019-10-16 14:16:22 +0300

    add convert in struct

--- fillit.h
+++ fillit.h
@@ -6,16 +6,17 @@
 /*   By: ojustine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 14:33:12 by ojustine          #+#    #+#             */
-/*   Updated: 2019/10/14 16:19:24 by ojustine         ###   ########.fr       */
+/*   Updated: 2019/10/16 11:55:03 by ojustine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-# define MALLCHECK(x) if (!(x)) return (NULL);
-# define ISNEG(X) (((X) < 0) ? (1) : (0))
+# define X 0
+# define Y 1
+# define CHECK_NULL(x) if (!(x)) return (NULL);
+# define CHECK_INT(x) if (!(x)) return (-1);
 
-//# include <limits.h>
 # include <stdlib.h>
 # include <string.h>
 # include <fcntl.h>
@@ -36,10 +37,4 @@ typedef struct		s_errors
 	int				dot;
 	int				i;
 }					t_errors;
-//# if (BUFF_SIZE < 1)
-//#  error "BUFF_SIZE must be strictly positive"
-//# endif
-//# if (BUFF_SIZE > SSIZE_MAX)
-//#  warning "BUFF_SIZE current value may cause a stack overflow"
-//# endif
 #endif
--- get_figs.c
+++ get_figs.c
@@ -6,26 +6,26 @@
 /*   By: ojustine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:16:11 by ojustine          #+#    #+#             */
-/*   Updated: 2019/10/15 14:05:47 by ojustine         ###   ########.fr       */
+/*   Updated: 2019/10/16 13:52:59 by ojustine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int	is_valid(char *buf)
+static int		is_valid(char *buf)
 {
 	t_errors err = {0, 0, 0, 0, 0};
 
+	CHECK_INT(buf[4] == '\n' && buf[9] == '\n' && buf[14] == '\n'
+	&& buf[19] == '\n');
 	while (buf[err.i])
 	{
 		if (buf[err.i] == '#')
 		{
 			err.obj++;
-			if (buf[err.i + 1] == '#' || (err.i > 0 && buf[err.i - 1] == '#'))
-				err.touch++;
-			if (err.i < 16 && buf[err.i + 5] == '#')
-				err.touch++;
-			if (err.i > 4 && buf[err.i - 5] == '#')
+			if (buf[err.i + 1] == '#' || (err.i > 0 && buf[err.i - 1] == '#')
+			|| (err.i < 16 && buf[err.i + 5] == '#')
+			|| (err.i > 4 && buf[err.i - 5] == '#'))
 				err.touch++;
 		}
 		else if (buf[err.i] == '.')
@@ -41,16 +41,46 @@ static int	is_valid(char *buf)
 	return (1);
 }
 
-t_fig		*get_next_fig(int fd)
+static t_fig	*conv_str_to_fig(char *buf, char sym)
 {
 	t_fig	*fig;
-	t_fig	test = {.sym = 'a', .next = NULL};
+	int		i;
+	int		j;
+	int		shift_x;
+	int		shift_y;
+
+	CHECK_NULL(fig = (t_fig*)malloc(sizeof(t_fig)));
+	fig->sym = sym;
+	shift_x = shift_fig(buf, X);
+	shift_y = shift_fig(buf, Y);
+	i = 0;
+	while (++i < 4)
+	{
+		j = 0;
+		while (++j < 4)
+		{
+			if (buf[i * 5 + j] == '#') {
+				fig->points[i][X] = (j - shift_x);
+				fig->points[i][Y] = (i - shift_y);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (fig);
+}
+
+t_fig			*get_next_fig(int fd)
+{
+	t_fig	*fig;
 	char	buf[22];
 	int		reads;
 
 	CHECK_NULL((reads = read(fd, buf, 21) > 19);
+	if (reads == 20)
+		reads++
 	buf[reads] = '\0';
 	CHECK_NULL(is_valid(buf));
-	CHECK_NULL(fig = conv_to_struct(buf));
+	CHECK_NULL(fig = conv_str_to_fig(buf));
 	return (fig);
 }
