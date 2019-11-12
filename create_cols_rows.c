a47b80e82d9c900d28f3d3437c64897f2f274891
Ojustine <!Lexx7979ova>
2019-11-12 17:45:25 +0300

    bb

--- create_cols_rows.c
+++ create_cols_rows.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   create_cols_rows.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojustine <ojustine@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ojustine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 11:44:30 by ojustine          #+#    #+#             */
 /*   Updated: 2019/11/05 12:41:47 by ojustine         ###   ########.fr       */
@@ -12,6 +12,20 @@
 
 #include "fillit.h"
 
+/*
+**	Accepts the address of the pointer for future columns and the proposed size
+**	of the future solution. At the address creates an array of pointers to a
+**	structure of type t_col. The size of the array is the number of cells of the
+**	proposed solution (i.e., size^2). For each pointer from the array,
+**	a structure of type t_col is created, which has the Name and Length fields.
+**	One column is one corresponding cell of a possible solution (for a given
+**	size). Columns are named from 1 to size^2. All columns are added to a
+**	circular doubly linked list.
+**	An example with the size of the proposed solution 3:
+**							|1|2|3|    cols[0] = {.length = 0, .name = 1}
+**	cols[size * size]   =>	|4|5|6| => cols[1] = {.length = 0, .name = 2}
+**							|7|8|9|     ... and so on
+*/
 static void		create_cols(t_col ***cols, int size)
 {
 	int		i;
@@ -104,6 +118,15 @@ static t_row	*create_rows(t_row *figs_lst, int size)
 	else
 		return (NULL);
 }
+
+/*
+**	Accepts a pointer to a list of figures, a pointer address for future columns
+**	and a solution size address. Creates columns at the received address.
+**	Creates rows and the root row (empty) included in the row list. Returns a
+**	pointer to the root. If necessary, the minimum required size of the future
+**	solution is increased.
+**	More details in the description of inner functions.
+*/
 
 t_row			*create_cols_rows(t_row *figs_lst, t_col ***cols_ptr, int *size)
 {
